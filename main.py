cd4da304197295d1b507b3489b88feadd164b2aa
Satyam Vishwakarma <officialsatyamvishwakarma06@gmail.com>
2026-01-28 16:16:31 +0530

    added ignore file

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
 st.title(f"Welcome, {st.session_state.username}!", text_alignment="center")
-st.write("You are now logged in.", text_alignment="center")
-st.write("Select an option from the sidebar to continue.", text_alignment="center")
+st.write("You are now logged in")
+st.write("Select an option from the sidebar to continue")
